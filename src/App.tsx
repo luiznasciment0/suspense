b32a8774dc9d6738882b9d6b8161f43aa36e15c1
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-08-29 20:32:42 -0300

    update App with resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { fetchUserProfileData } from './utils/fetchApi';
+import { Resource } from './types/resource';
+import { wrapPromise } from './utils/wrapPromise';
+
 import './App.css';
+import { UserProfileList } from './components/UserProfileList/UserProfileList';
+
+const resource: Resource = {
+  1: wrapPromise(fetchUserProfileData(1)),
+  2: wrapPromise(fetchUserProfileData(2)),
+  3: wrapPromise(fetchUserProfileData(3))
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProfileList resource={resource} />
     </div>
   );
 }
