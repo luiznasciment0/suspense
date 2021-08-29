e8475c3226b35b7cde7aca3c21283c93b1a993bf
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-08-29 20:31:27 -0300

    create user types

---
+++ src/types/userType.ts
@@ -0,0 +1,37 @@
+// Extracted from https://docs.github.com/en/rest/reference/users#get-a-user
+export type User = {
+  "login": string,
+  "id": number,
+  "node_id": string,
+  "avatar_url": string,
+  "gravatar_id": string,
+  "url": string
+  "html_url": string,
+  "followers_url": string,
+  "following_url": string,
+  "gists_url": string,
+  "starred_url": string,
+  "subscriptions_url": string,
+  "organizations_url": string,
+  "repos_url": string,
+  "events_url": string,
+  "received_events_url": string,
+  "type": string,
+  "site_admin": false,
+  "name": string,
+  "company": string,
+  "blog": string,
+  "location": string,
+  "email": string,
+  "hireable": boolean,
+  "bio": string,
+  "twitter_username": string,
+  "public_repos": number,
+  "public_gists": number,
+  "followers": number,
+  "following": number,
+  "created_at": string,
+  "updated_at": string
+}
+
+export type UserId = 1 | 2 | 3
